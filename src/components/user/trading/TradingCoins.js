96542bca1568b52688348a4cf4a87b744eb6548e
Sebahammer97 <55815143+Sebahammer97@users.noreply.github.com>
2021-06-03 15:02:29 -0300

    Creación de histograma de las cripto

--- src/components/user/trading/TradingCoins.js
+++ src/components/user/trading/TradingCoins.js
@@ -2,47 +2,90 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-  coinsMarketsAPI, 
-  infoCoinMarketPrice, 
-  setSelectedCoinId
+  coinsMarketsAPI,
+  infoCoinMarketPrice,
+  setSelectedCoinId,
 } from "../../../reducers/tradingReducer";
-
-
 
 export const TradingCoins = () => {
   const dispatch = useDispatch();
 
+  const listFilters = [
+    { label: "1 Día", days: 1 },
+    { label: "7 Días", days: 7 },
+    { label: "15 Días", days: 15 },
+    { label: "30 Días", days: 30 },
+    { label: "3 Meses", days: 90 },
+    { label: "6 Meses", days: 180 },
+    { label: "1 Año", days: 365 },
+  ];
+
   const listCoins = useSelector((state) => state.trading.marketCoins);
   const [coinselect, setCoinSelect] = useState("");
+  const [filterSelect, setFilterSelect] = useState(listFilters[0]);
 
   useEffect(() => {
     dispatch(coinsMarketsAPI());
   }, [dispatch]);
 
-
-  const handleClickCoin = (id) =>{
-    setCoinSelect(id) ;
+  const handleClickCoin = (id) => {
+    setCoinSelect(id);
     dispatch(setSelectedCoinId(id));
-    dispatch(infoCoinMarketPrice(1));
-  }
+    dispatch(infoCoinMarketPrice(filterSelect));
+  };
 
+  const handleClickFilter = (days) => {
+    setFilterSelect(days);
+    if (coinselect && days) dispatch(infoCoinMarketPrice(days));
+  };
+
+  if (!coinselect && listCoins && listCoins.length > 0) {
+    setCoinSelect(listCoins[0].id);
+    setFilterSelect(listFilters[0].days);
+    dispatch(setSelectedCoinId(listCoins[0].id));
+    dispatch(infoCoinMarketPrice(listFilters[0].days));
+  }
 
   return (
     <div className="trading__coins">
       {/* las monedas que podes ver en el mercado, poner un máximo de 5 */}
+      <h2 className="trading__coins-title">Seleccione una criptomenda:</h2>
       <section className="trading__coins-container">
-
         {listCoins.length > 0 && (
           <div className="trading__coins-group">
             {listCoins.map((coin) => (
               <div
                 onClick={() => handleClickCoin(coin.id)}
-                className={`trading__coins-name ${coinselect === coin.id ? "trading__coins-name-active" : ""}`}
+                className={`trading__coins-name ${
+                  coinselect === coin.id ? "trading__coins-name-active" : ""
+                }`}
                 key={coin.id}
               >
                 <img src={coin.image} alt={coin.name} />
                 <section className="trading__coins-name-s1">
                   <span>{coin.name}</span>
+                </section>
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+      <h2 className="trading__coins-title">Filtros:</h2>
+      <section className="trading__coins-container">
+        {listFilters.length > 0 && (
+          <div className="trading__coins-group">
+            {listFilters.map((filter) => (
+              <div
+                onClick={() => handleClickFilter(filter.days)}
+                className={`trading__coins-filter ${
+                  filterSelect === filter.days
+                    ? "trading__coins-filter-active"
+                    : ""
+                }`}
+                key={filter.label}
+              >
+                <section className="trading__coins-filter-label">
+                  <span>{filter.label}</span>
                 </section>
               </div>
             ))}
--- src/components/user/trading/TradingMarket.js
+++ src/components/user/trading/TradingMarket.js
@@ -1,50 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { LineChart } from '../../linechart/LineChart';
-
-
-
-/*  const data = [
-   { label: "S", x: 0, y: 0 },
-   { label: "M", x: 1, y: 400 },
-   { label: "T", x: 2, y: 300 },
-   { label: "W", x: 3, y: 100 },
-   { label: "TH", x: 4, y: 400 },
-   { label: "F", x: 5, y: 500 },
-   { label: "S", x: 6, y: 400 },
-   { label: "S", x: 7, y: 1110 },
-   { label: "M", x: 8, y: 600 },
-   { label: "T", x: 9, y: 900 },
-   { label: "W", x: 10, y: 200 },   
-];  */
-
-
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { LineChart } from "../../linechart/LineChart";
 
 export const TradingMarket = () => {
+  const priceCoin = useSelector((state) => state.trading.priceCoin);
+  const [data, setData] = useState({});
 
-    const priceCoin = useSelector(state => state.trading.priceCoin);
-    const [data, setData] = useState({});
+  useEffect(() => {
+    setData(priceCoin);
+  }, [priceCoin]);
 
-    useEffect(() => {        
-        setData(priceCoin);
-        console.log(data);
-    }, [priceCoin]) 
-
-
-    return (
-        <>
-            {data.length > 0 && (
-                <div className="tradingMarket">
-                    <LineChart
-                        width={900}
-                        height={550}
-                        data={data}
-                        horizontalGuides={10}
-                        precision={2}
-                        verticalGuides={1}
-                    />
-                </div>
-            )}
-        </>
-    );
-}
+  return (
+    <>
+      <div className="tradingMarket">
+        <LineChart
+          index={data.index}
+          price={data.price}
+          volumes={data.volumes}
+        />
+      </div>
+    </>
+  );
+};
