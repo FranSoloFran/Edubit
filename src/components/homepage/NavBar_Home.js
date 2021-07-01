ed4911c668b8ce959b406f4634c5acb3ea3c53c2
Sebahammer97 <55815143+Sebahammer97@users.noreply.github.com>
2021-06-30 22:32:24 -0300

    Cambios de resources

--- src/components/homepage/NavBar_Home.js
+++ src/components/homepage/NavBar_Home.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const NavBarHome = () => {
-
-    return (
-        <aside className="home__sidebar">
-            <div className="home__sidebar-navbar">
-                <div className="home__sidebar-logo mt-1"></div>
-                <Link to="/homepage/login" className="home__sidebar-link">Login</Link>
-            </div>
-
-        </aside>
-
-    )
-}
+  return (
+    <aside className="home__sidebar">
+      <div className="home__sidebar-navbar">
+        <div className="home__sidebar-logo mt-1"></div>
+        <Link to="/homepage/login" className="home__sidebar-link">
+          Iniciar sesión
+        </Link>
+      </div>
+    </aside>
+  );
+};
