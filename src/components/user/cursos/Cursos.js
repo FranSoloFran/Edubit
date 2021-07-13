cd900deedf30cb760e315cb71855a2072880e289
Rodrigo Baliani <rodro96@gmail.com>
2021-07-12 22:59:35 -0300

    Fix css

--- src/components/user/cursos/Cursos.js
+++ src/components/user/cursos/Cursos.js
@@ -25,12 +25,14 @@ export const Cursos = () => {
               <div className="cursosLista__card">
                 <h3>Criptomonedas 101 - Nivel Básico</h3>
                 <br />
-                <ol>
-                  <li><span>Criptomonedas: ¿Qué son?</span></li>
-                  <li><span>Errores comunes al invertir en criptomonedas</span></li>
-                  <li><span>Criptomonedas más populares</span></li>
-                  <li><span>¿Qué es el Bitcoin?</span></li>
-                </ol>
+                <div className="cursos__container_temario">
+                  <ol>
+                    <li><span>Criptomonedas: ¿Qué son?</span></li>
+                    <li><span>Errores comunes al invertir en criptomonedas</span></li>
+                    <li><span>Criptomonedas más populares</span></li>
+                    <li><span>¿Qué es el Bitcoin?</span></li>
+                  </ol>
+                </div>
                 <br />
                 <Link to="/landingpage/cursos/cursoBasico">
                   <button className="cursosLista__button_color1">Ir al curso</button>
@@ -41,12 +43,14 @@ export const Cursos = () => {
               <div className="cursosLista__card">
                 <h3>Criptomonedas 101 - Nivel Intermedio 1</h3>
                 <br />
-                <ol>
-                  <li><span>¿Qué determina el precio de una criptomoneda?</span></li>
-                  <li><span>Cuestiones legales de las criptomonedas</span></li>
-                  <li><span>Cómo evitar inconvenientes legales</span></li>
-                  <li><span>Comprar/vender criptomonedas en Argentina</span></li>
-                </ol>
+                <div className="cursos__container_temario">
+                  <ol>
+                    <li><span>¿Qué determina el precio de una criptomoneda?</span></li>
+                    <li><span>Cuestiones legales de las criptomonedas</span></li>
+                    <li><span>Cómo evitar inconvenientes legales</span></li>
+                    <li><span>Comprar/vender criptomonedas en Argentina</span></li>
+                  </ol>
+                </div>
                 <br />
                 <Link to="/landingpage/cursos/cursoIntermedio1">
                   <button className="cursosLista__button_color1">Ir al curso</button>
@@ -57,12 +61,14 @@ export const Cursos = () => {
               <div className="cursosLista__card">
                 <h3>Criptomonedas 101 - Nivel Intermedio 2</h3>
                 <br />
-                <ol>
-                  <li><span>Tecnología y Seguridad de las criptomonedas</span></li>
-                  <li><span>Cómo almacenar criptomonedas</span></li>
-                  <li><span>Diferencias entre criptomoneda y token</span></li>
-                  <br />
-                </ol>
+                <div className="cursos__container_temario">
+                  <ol>
+                    <li><span>Tecnología y Seguridad de las criptomonedas</span></li>
+                    <li><span>Cómo almacenar criptomonedas</span></li>
+                    <li><span>Diferencias entre criptomoneda y token</span></li>
+                    <br />
+                  </ol>
+                </div>
                 <br />
                 <Link to="/landingpage/cursos/cursoIntermedio2">
                   <button className="cursosLista__button_color1">Ir al curso</button>
@@ -73,12 +79,14 @@ export const Cursos = () => {
               <div className="cursosLista__card">
                 <h3>Criptomonedas 101 - Avanzado</h3>
                 <br />
-                <ol>
-                  <li><span>¿Cuáles son las estrategias de trading para distintos perfiles de inversión?</span></li>
-                  <li><span>Acciones vs Criptomonedas</span></li>
-                  <li><span>¿Cuáles son algunas técnicas para la compra/venta?</span></li>
-                  <br />
-                </ol>
+                <div className="cursos__container_temario">
+                  <ol>
+                    <li><span>¿Cuáles son las estrategias de trading para distintos perfiles de inversión?</span></li>
+                    <li><span>Acciones vs Criptomonedas</span></li>
+                    <li><span>¿Cuáles son algunas técnicas para la compra/venta?</span></li>
+                    <br />
+                  </ol>
+                </div>
                 <br />
                 <Link to="/landingpage/cursos/cursoAvanzado">
                   <button className="cursosLista__button_color1">Ir al curso</button>
